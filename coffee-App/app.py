a39d0923ec08db880f69b7cc1db6b878f54de3d2
adnan <adnan.victor@student.moringaschool.com>
2024-06-12 09:09:36 +0300

    bean model complete

--- coffee-App/app.py
+++ coffee-App/app.py
@@ -1,6 +1,17 @@
-import database
+import Bean_model
 
 MENU_PROMPT="""
+-- Coffee Bean App --
+
+Please choose one of these options:
+
+1) View coffee beans.
+2) View Coffee shops
+3) Exit.
+
+Your selection: """
+
+BEAN_MENU = """
 -- Coffee Bean App --
 
 Please choose one of these options:
@@ -14,10 +25,21 @@ Please choose one of these options:
 Your selection: """
 
 def menu():
-    connection = database.connect()
-    database.create_tables(connection)
+    connection = Bean_model.connect()
+    Bean_model.create_tables(connection)
 
-    while(user_input := input(MENU_PROMPT)) != "5":
+    while(user_input := input(MENU_PROMPT)) != "3":
+        if user_input == "1":
+            bean_menu()
+        elif user_input == "2":
+            pass
+        else:
+            print("Invalid input, please try again.")
+
+def bean_menu():
+    connection = Bean_model.connect()
+    Bean_model.create_tables(connection)
+    while(user_input := input(BEAN_MENU)) != "5":
         if user_input == "1":
            add_bean(connection)
 
@@ -32,27 +54,28 @@ def menu():
         else:
             print("Invalid input, please try again.")
 
+
 def add_bean(connection):
     name = input("Enter bean name: ")
     method = input("Enter how you've prepared it: ")
     rating = int(input("Enter your rating score (0-100): "))
-    database.add_bean(connection,name, method, rating)
+    Bean_model.add_bean(connection,name, method, rating)
 
 def get_all_beans(connection):
-    beans = database.get_all_beans(connection)
+    beans = Bean_model.get_all_beans(connection)
     for bean in beans:
         print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
 
 def find_bean(connection):
     name = input("Enter bean name to find: ")
-    beans = database.get_beans_by_name(connection, name)
+    beans = Bean_model.get_beans_by_name(connection, name)
 
     for bean in beans:
         print (f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
 
 def find_best_method(connection):
     name = input("Enter bean name to find: ")
-    best_method = database.best_preparation_for_bean(connection,name)
+    best_method = Bean_model.best_preparation_for_bean(connection,name)
 
     print(f"The best preparation method for {name} is {best_method[2]} ")
 
